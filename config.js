740f3e418b644f4161c71d78eee56a8d9e7a3934
Brendan Tanner <btan579@gmail.com>
2018-07-20 15:43:49 -0500

    "updated config notes"

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ exports.DATABASE_URL =
 exports.PORT = process.env.PORT || 8080;
 // mongoimport --db blogs-app --collection blogs --drop --file C:/Users/btan/Projects/blog-api-challenge/blog-database/seed-data.json
 //  mongodb: //user1:password1@ds145981.mlab.com:45981/blog-api-challenge-db
-// mongo ds145981.mlab.com:45981/blog-api-challenge -db -u user1  -p password1
+// mongo ds145981.mlab.com:45981/blog-api-challenge-db -u user1 -p password1
 // mongoimport -h ds145981.mlab.com:45981 -d blog-api-challenge-db -c blogs -u user1 -p password1 --file C:/Users/btan/Projects/blog-api-challenge/blog-database/seed-data.json
 
 // exports.TEST_DATABASE_URL =
